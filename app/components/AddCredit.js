05e5ab4d468669526c893a2ba132ff7add74f9f6
pepsin92 <pepsin92@users.noreply.github.com>
2017-03-26 22:49:22 +0200

    Implement money withdrawal (#3)
    * Implement money withdrawal
    
    * Fix button text

--- app/components/AddCredit.js
+++ app/components/AddCredit.js
@@ -7,7 +7,7 @@ export class AddCredit extends Component {
     e.preventDefault();
     const {username, fetchUsers, balance} = this.props;
 
-    if (balance == null || balance <= 0) {
+    if (balance == null) {
       window.alert('Neplatná hodnota!');
       return;
     }
@@ -32,7 +32,7 @@ export class AddCredit extends Component {
         <Row>
           <Col lg={8} md={8} sm={8}>
             <Panel>
-              <PageHeader>Pridať kredit</PageHeader>
+              <PageHeader>Pridať kredit / vybrať hotovosť</PageHeader>
               <form onSubmit={(e) => this.addCredit(e)}>
                 <Row>
                   <Col lg={6} md={6} sm={6}>
@@ -45,12 +45,15 @@ export class AddCredit extends Component {
                       />
                   </Col>
                   <Col lg={4} md={4} sm={4}>
-                    <Button bsStyle={'success'} type={'submit'} disabled={!(balance > 0)}>
-                      Pridaj kredit
+                    <Button bsStyle={'success'}
+                      type={'submit'}
+                      disabled={isNaN(balance) || (balance == 0)}
+                      >Pridaj kredit / vyber hotovosť
                     </Button>
                   </Col>
                   <Col lg={2} md={2} sm={2} />
                 </Row>
+                <p>Ak chceš vybrať hotovosť zo svojho účtu, napíš zápornú hodnotu.</p>
               </form>
             </Panel>
           </Col>
--- app/components/Sidebar.js
+++ app/components/Sidebar.js
@@ -34,10 +34,11 @@ export class Sidebar extends Component {
                     block
                     >Obchod</Button>
                   <Button
+                    styleName={'credit'}
                     bsStyle={'primary'}
                     onClick={() => goToPage(pages.addCredit)}
                     block
-                    >Nabi kredit</Button>
+                    >Nabi kredit <br />vyber hotovosť</Button>
                   <Button
                     bsStyle={'primary'}
                     onClick={() => goToPage(pages.addStock)}
